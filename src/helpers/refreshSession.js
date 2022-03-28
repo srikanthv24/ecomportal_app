2b79030fd1bd0dd86f653d196700e79049e3db48
Shahnawaz Alam <shahnawaz90.alam@gmail.com>
2022-03-28 12:20:38 +0530

    token issue fixed

--- src/helpers/refreshSession.js
+++ src/helpers/refreshSession.js
@@ -2,14 +2,17 @@ import auth_services from "../services/auth_services";
 
 export class RefreshToken {
   static getRefreshedToken = async () => {
+    let token = "";
     const lastRefreshTime = localStorage.getItem('expiry-time');
-    const currentTime = Date.now();
-    const diffInMin = (currentTime - lastRefreshTime) / 1000 /60;
-    let token = localStorage.getItem('token');
-    if (diffInMin >= 5) {
-      const newToken = await auth_services.getUserToken();
-      token = newToken.accessToken.jwtToken;
-      localStorage.setItem('expiry-time', Date.now());
+    token = localStorage.getItem('token');
+    if(lastRefreshTime && token) {
+      const currentTime = Date.now();
+      const diffInMin = (currentTime - lastRefreshTime) / 1000 /60;
+      if (diffInMin >= 5) {
+        const newToken = await auth_services.getUserToken();
+        token = newToken.accessToken.jwtToken;
+        localStorage.setItem('expiry-time', Date.now());
+      }
     }
     return token;
   }
