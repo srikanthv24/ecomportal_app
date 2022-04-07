94d7390af9372c208ceaa7d6170af35cac2804ee
Shahnawaz Alam <shahnawaz90.alam@gmail.com>
2022-04-07 15:59:46 +0530

    added address to cart payload

--- src/scenes/VibrantMealPlanner/index.js
+++ src/scenes/VibrantMealPlanner/index.js
@@ -133,6 +133,7 @@ function VibrantMealPlanner() {
           customerId,
           productId,
           selectedSessions,
+          address
         })
       );
       history.push("/cart-summary");
