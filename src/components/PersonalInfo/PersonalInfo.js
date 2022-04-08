5165619c3bf0dd99f0d01f5258e7dd0f48d54672
Srikanth <srikanth.vemalla@mobigesture.com>
2022-04-09 00:58:57 +0530

    #1 working on Personal info screen

--- src/components/PersonalInfo/PersonalInfo.js
+++ src/components/PersonalInfo/PersonalInfo.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./personalInfo.scss";
 import { MdMale, MdFemale } from "react-icons/md";
 import { Button, Card } from "react-bootstrap";
 import { GrAdd, GrSubtract } from "react-icons/gr";
 import { NEXT, PERSONAL_INFO } from "../../utils/constants";
+import handleValidations from "../../utils/handleValidations";
 
 const PersonalInfo = ({
   handleBack,
@@ -15,7 +16,6 @@ const PersonalInfo = ({
   defaultAge,
   onProfileDetailsSubmit,
 }) => {
-  
   const [gender, setGender] = useState(defaultGender);
   let [heightFeet, setHeightFeet] = useState(defaultHeightFeet);
   const [heightInch, setHeightInch] = useState(defaultHeightInch);
@@ -33,34 +33,115 @@ const PersonalInfo = ({
     handleNextStep();
   };
 
+  const [error, setError] = useState({
+    ageErrorMsg: "",
+    ageError: false,
+    weigthErrorMsg: "",
+    weightError: false,
+    heigthFeetErrorMsg: "",
+    heigthFeetError: false,
+    heigthInchErrorMsg: "",
+    heigthInchError: false,
+  });
+
+  const AgeValidator = (age, type) => {
+    const { error, msg } = handleValidations.ageValidation(age, type);
+    if (error) {
+      setError((err) => ({ ...err, ageError: true, ageErrorMsg: msg }));
+    } else {
+      type === "increment"
+        ? setAge((prevAge) => prevAge + 1)
+        : setAge((prevAge) => prevAge - 1);
+      setError((err) => ({ ...err, ageError: false, ageErrorMsg: "" }));
+    }
+  };
+
+  const weigthValidator = (weigth, type) => {
+    const { error, msg } = handleValidations.weigthValidation(weigth, type);
+    if (error) {
+      setError((err) => ({ ...err, weightError: true, weigthErrorMsg: msg }));
+    } else {
+      type === "increment"
+        ? setWeight((prevWeigth) => prevWeigth + 1)
+        : setWeight((prevWeigth) => prevWeigth - 1);
+      setError((err) => ({ ...err, weightError: false, weigthErrorMsg: "" }));
+    }
+  };
+
+  const heigthFeetValidator = (heightFeet, type) => {
+    const { error, msg } = handleValidations.heightFeetValidation(
+      heightFeet,
+      type
+    );
+    if (error) {
+      setError((err) => ({
+        ...err,
+        heigthFeetError: true,
+        heightFeetErrorMsg: msg,
+      }));
+    } else {
+      type === "increment"
+        ? setHeightFeet((prevHeigthFeet) => prevHeigthFeet + 1)
+        : setHeightFeet((prevHeigthFeet) => prevHeigthFeet - 1);
+      setError((err) => ({
+        ...err,
+        heigthFeetError: false,
+        heightFeetErrorMsg: "",
+      }));
+    }
+  };
+
+  const heigthInchValidator = (heightInch, type) => {
+    const { error, msg } = handleValidations.heightInchValidation(
+      heightInch,
+      type
+    );
+    if (error) {
+      setError((err) => ({
+        ...err,
+        heigthInchError: true,
+        heightInchErrorMsg: msg,
+      }));
+    } else {
+      type === "increment"
+        ? setHeightInch((prevHeigthInch) => prevHeigthInch + 1)
+        : setHeightInch((prevHeigthInch) => prevHeigthInch - 1);
+      setError((err) => ({
+        ...err,
+        heigthInchError: false,
+        heightInchErrorMsg: "",
+      }));
+    }
+  };
+
   const handleIncrement = (key) => {
     if (key === "heightFeet") {
-      setHeightFeet(heightFeet + 1);
+      heigthFeetValidator(heightFeet, "increment");
     } else if (key === "heightInch") {
-      setHeightInch(heightInch + 1);
+      heigthInchValidator(heightInch, "increment");
     } else if (key === "weight") {
-      setWeight(weight + 1);
+      weigthValidator(weight, "increment");
     } else if (key === "age") {
-      setAge(age + 1);
+      AgeValidator(age, "increment");
     }
   };
 
   const handleDecrement = (key) => {
     if (key === "heightFeet") {
-      setHeightFeet(heightFeet - 1);
+      heigthFeetValidator(heightFeet, "decrement");
     } else if (key === "heightInch") {
-      setHeightInch(heightInch - 1);
+      heigthInchValidator(heightInch, "decrement");
     } else if (key === "weight") {
-      setWeight(weight - 1);
+      weigthValidator(weight, "decrement");
     } else if (key === "age") {
-      setAge(age - 1);
+      AgeValidator(age, "decrement");
     }
   };
 
+  useEffect(() => {}, [error]);
+
   return (
-    <section
-      className="text-center w-100p mx-auto"
-    >
+    <section className="text-center w-100p mx-auto">
       <div className="py-4">
         <Card.Body className="py-3 px-0 d-flex justify-content-between">
           <label class="gender-card justify-content-center">
@@ -79,8 +160,7 @@ const PersonalInfo = ({
               }}
             >
               <span class="plan-cost">
-                <MdMale
-                />
+                <MdMale />
               </span>
             </span>
           </label>
@@ -107,104 +187,157 @@ const PersonalInfo = ({
         </Card.Body>
 
         <Card.Body className="py-3 px-0 d-flex justify-content-between">
-           <div className="pinfo-box">
-              <div className="text-muted">
+          <div className="pinfo-box">
+            <div className="text-muted">
               <label>{PERSONAL_INFO.HEIGHT_FEET}</label>
-              </div>
-              <div className="pinfo-box-item">
-              <button
-              type="button"
-              className="pinfo-box-item-btn"
-              onClick={() => handleDecrement("heightFeet")}
-            >
-              <GrSubtract />
-            </button>
-            <input type="number" min="1" max="9" value={heightFeet} readOnly />
-            <button
-              type="button"
-              className="pinfo-box-item-btn"
-              onClick={() => handleIncrement("heightFeet")}
-            >
-              <GrAdd />
-            </button>
-              </div>
             </div>
+            <div className="pinfo-box-item">
+              <button
+                type="button"
+                className="pinfo-box-item-btn"
+                onClick={() => handleDecrement("heightFeet")}
+              >
+                <GrSubtract />
+              </button>
+              <input
+                type="number"
+                min="1"
+                max="9"
+                value={heightFeet}
+                readOnly
+                style={{ display: "none" }}
+              />
+              <div style={{ width: "100%", height: "100%", fontSize: "50px" }}>
+                {heightFeet}
+              </div>
+              <button
+                type="button"
+                className="pinfo-box-item-btn"
+                onClick={() => handleIncrement("heightFeet")}
+              >
+                <GrAdd />
+              </button>
+            </div>
+            <p>{error.heightFeetErrorMsg}</p>
+          </div>
 
-            <div className="pinfo-box">
-              <div className="text-muted">
+          <div className="pinfo-box">
+            <div className="text-muted">
               <label>{PERSONAL_INFO.HEIGHT_INCHES}</label>
-              </div>
-              <div className="pinfo-box-item">
-              <button
-              type="button"
-              className="pinfo-box-item-btn"
-              onClick={() => handleDecrement("heightInch")}
-            >
-              <GrSubtract />
-            </button>
-            <input type="number" min="1" max="9" step="1" value={heightInch} />
-            <button
-              type="button"
-              className="pinfo-box-item-btn"
-              onClick={() => handleIncrement("heightInch")}
-            >
-              <GrAdd />
-            </button>
-              </div>
             </div>
-        
+            <div className="pinfo-box-item">
+              <button
+                type="button"
+                className="pinfo-box-item-btn"
+                onClick={() => handleDecrement("heightInch")}
+              >
+                <GrSubtract />
+              </button>
+              <input
+                type="number"
+                min="1"
+                max="9"
+                step="1"
+                value={heightInch}
+                style={{ display: "none" }}
+              />
+              <div style={{ width: "100%", height: "100%", fontSize: "50px" }}>
+                {heightInch}
+              </div>
+              <button
+                type="button"
+                className="pinfo-box-item-btn"
+                onClick={() => handleIncrement("heightInch")}
+              >
+                <GrAdd />
+              </button>
+            </div>
+            <p>{error.heightInchErrorMsg}</p>
+          </div>
         </Card.Body>
 
         <Card.Body className="py-3 px-0 d-flex justify-content-between">
-           <div className="pinfo-box">
-              <div className="text-muted">
+          <div className="pinfo-box">
+            <div className="text-muted">
               <label>{PERSONAL_INFO.WEIGHT}</label>
-              </div>
-              <div className="pinfo-box-item">
-              <button
-              type="button"
-              className="pinfo-box-item-btn"
-              onClick={() => handleDecrement("weight")}
-            >
-              <GrSubtract />
-            </button>
-            <input type="number" min="1" max="9" step="1" value={weight} />
-            <button
-              type="button"
-              className="pinfo-box-item-btn"
-              onClick={() => handleIncrement("weight")}
-            >
-              <GrAdd />
-            </button>
-              </div>
             </div>
-
-            <div className="pinfo-box">
-              <div className="text-muted">
-              <label>{PERSONAL_INFO.AGE}</label>
-              </div>
-              <div className="pinfo-box-item">
+            <div className="pinfo-box-item">
               <button
-              type="button"
-              className="pinfo-box-item-btn"
-              onClick={() => handleDecrement("age")}
-            >
-              <GrSubtract />
-            </button>
-            <input type="number" value={age} min="1" max="100" />
-            <button
-              type="button"
-              className="pinfo-box-item-btn"
-              onClick={() => handleIncrement("age")}
-            >
-              <GrAdd />
-            </button>
+                type="button"
+                className="pinfo-box-item-btn"
+                onClick={() => handleDecrement("weight")}
+              >
+                <GrSubtract />
+              </button>
+              <input
+                type="number"
+                min="1"
+                max="9"
+                step="1"
+                value={weight}
+                style={{ display: "none" }}
+              />
+              <div style={{ width: "100%", height: "100%", fontSize: "50px" }}>
+                {weight}
               </div>
-            </div>        
+              <button
+                type="button"
+                className="pinfo-box-item-btn"
+                onClick={() => handleIncrement("weight")}
+              >
+                <GrAdd />
+              </button>
+            </div>
+            <p>{error.weigthErrorMsg}</p>
+          </div>
+
+          <div className="pinfo-box">
+            <div className="text-muted">
+              <label>{PERSONAL_INFO.AGE}</label>
+            </div>
+            <div className="pinfo-box-item">
+              <button
+                type="button"
+                className="pinfo-box-item-btn"
+                onClick={() => handleDecrement("age")}
+              >
+                <GrSubtract />
+              </button>
+              <input
+                type="number"
+                value={age}
+                min="1"
+                max="100"
+                style={{ display: "none" }}
+              />
+              <div style={{ width: "100%", height: "100%", fontSize: "50px" }}>
+                {age}
+              </div>
+              <button
+                type="button"
+                className="pinfo-box-item-btn"
+                onClick={() => handleIncrement("age")}
+              >
+                <GrAdd />
+              </button>
+            </div>
+            <p>{error.ageErrorMsg}</p>
+          </div>
         </Card.Body>
       </div>
       <div className="stepper-btn-container">
-        <Button className="w-100 m-1 stepper-btn" onClick={onSubmit}>
+        <Button
+          className="w-100 m-1 stepper-btn"
+          onClick={onSubmit}
+          disabled={
+            error.ageError ||
+            error.weightError ||
+            error.heigthFeetError ||
+            error.heigthInchError
+              ? true
+              : false
+          }
+        >
           {NEXT}
         </Button>
       </div>
---
+++ src/utils/handleValidations.js
@@ -0,0 +1,74 @@
+export class handleValidations {
+  static ageValidation(age, type) {
+    let newAge = 0;
+    newAge = type === "increment" ? age + 1 : age -1;
+    let msg = "",
+      error = false;
+    if (newAge >= 65) {
+      error = true;
+      msg = "Please enter a valid age";
+    } else if (newAge <= 55) {
+      error = true;
+      msg = "Please enter a valid age";
+    }
+    return {
+      error,
+      msg,
+    };
+  }
+
+  static weigthValidation(weigth, type) {
+    let newWeigth = 0;
+    newWeigth = type === "increment" ? weigth + 1 : weigth -1;
+    let msg = "",
+      error = false;
+    if (newWeigth >= 65) {
+      error = true;
+      msg = "Enter valid weigth";
+    } else if (newWeigth <= 55) {
+      error = true;
+      msg = "Enter valid weigth";
+    }
+    return {
+      error,
+      msg,
+    };
+  }
+
+  static heightFeetValidation(heightFeet, type) {
+    let newHeightFeet = 0;
+    newHeightFeet = type === "increment" ? heightFeet + 1 : heightFeet -1;
+    let msg = "",
+      error = false;
+    if (newHeightFeet >= 12) {
+      error = true;
+      msg = "Enter valid height";
+    } else if (newHeightFeet <= 1) {
+      error = true;
+      msg = "Enter valid height";
+    }
+    return {
+      error,
+      msg,
+    };
+  }
+
+  static heightInchValidation(heightInch, type) {
+    let newHeightInch = 0;
+    newHeightInch = type === "increment" ? heightInch + 1 : heightInch -1;
+    let msg = "",
+      error = false;
+    if (newHeightInch >= 11 ) {
+      error = true;
+      msg = "Enter valid height";
+    } else if (newHeightInch <= 0 ) {
+      error = true;
+      msg = "Enter valid height";
+    }
+    return {
+      error,
+      msg,
+    };
+  }
+}
+export default handleValidations;
