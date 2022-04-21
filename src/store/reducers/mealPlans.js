6423b39634493b644734393ca03bc2d1eeb27486
Srikanth <srikanth.vemalla@mobigesture.com>
2022-04-21 15:06:57 +0530

    removed unwanted files in mealplans

--- src/store/reducers/mealPlans.js
+++ src/store/reducers/mealPlans.js
@@ -1,56 +1,31 @@
-import { types } from '../constants';
+import { types } from "../constants";
 
 const intialState = {
-    loading: false,
-    error: false,
-    mealPlanId: '',
-    mealPlansList: [],
-    details: {}
-}
+  loading: false,
+  error: false,
+  mealPlansList: [],
+};
 
 export const mealPlansReducer = (state = intialState, action) => {
-    switch (action.type) {
-        case types.MEAL_PLAN_ID:
-            return {
-                ...state,
-                mealPlanId: action.id,
-            }
-        case types.MEALPLANS_LIST:
-            return {
-                ...state,
-                loading: true,
-            }
-        case types.MEALPLANS_LIST_SUCCESS:
-                return {
-                    ...state,
-                    loading: false,
-                    mealPlansList: action.payload
-                }    
-        case types.MEALPLANS_LIST_FAILURE:
-                return {
-                    ...state,
-                    loading: false,
-                    mealPlansList: []
-                }    
-        case types.MEALPLAN_DETAILS:
-            return {
-                ...state,
-                loading: true,
-            }
-        case types.MEALPLAN_DETAILS_SUCCESS:
-                return {
-                    ...state,
-                    loading: false,
-                    details: action.payload
-                }    
-        case types.MEALPLAN_DETAILS_FAILURE:
-                return {
-                    ...state,
-                    details: {}
-                }    
-                
-        
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case types.MEALPLANS_LIST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case types.MEALPLANS_LIST_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        mealPlansList: action.payload,
+      };
+    case types.MEALPLANS_LIST_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        mealPlansList: [],
+      };
+    default:
+      return state;
+  }
+};
