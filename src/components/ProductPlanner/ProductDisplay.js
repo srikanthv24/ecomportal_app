733f2feeaa6467117df9c5e76e1de9e6ac9d5424
Pravalika Peram <agnivesh.marthala@mobigesture.com>
2022-04-21 15:38:52 +0530

    bugfix: fixing product display issues in product planner

--- src/components/ProductPlanner/ProductDisplay.js
+++ src/components/ProductPlanner/ProductDisplay.js
@@ -11,16 +11,22 @@ const ProductDisplay = React.memo(
     onClick,
     planName,
     subscriptionStartDate,
-    duration
+    duration,
   }) => {
     return (
       <div className="product-display" onClick={onClick}>
         <div className="prdTitle">{title}</div>
         <div className="prdCategory">{category}</div>
         <img src={imageUrl} alt="product-img"></img>
-        <div className="prdDesp">{description}</div>
-        <div className="prdDesp">Subscribed for {duration} day plan</div>
-        <div className="prdDesp">Start date: {getDateInTextFormat(subscriptionStartDate)}</div>
+        {description && <div className="prdDesp">{description}</div>}
+        {duration && (
+          <div className="prdDesp">Subscribed for {duration} day plan</div>
+        )}
+        {subscriptionStartDate && (
+          <div className="prdDesp">
+            Start date: {getDateInTextFormat(subscriptionStartDate)}
+          </div>
+        )}
       </div>
     );
   }
